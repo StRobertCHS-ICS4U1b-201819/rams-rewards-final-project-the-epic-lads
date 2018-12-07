d65be41fae8b119d570d85e12a9b3b5ef72b343a
robuntu <robuntu@stro.ycdsb.ca>
2018-12-07 10:37:09 -0500

    Start python file for kivy

--- RRTA-Derek.py
+++ RRTA-Derek.py
@@ -1,1 +1,9 @@
+# Choose from a list of rewards activities (activities will have predetermined point values) to rewards points for
+# (i.e Coding Club meeting, attend a basketball game, attend a dance.
 
+import kivy
+
+kivy.require('1.10.0')
+
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
