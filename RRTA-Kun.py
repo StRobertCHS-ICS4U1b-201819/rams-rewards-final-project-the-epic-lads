03fe3429ed566241be4f8b631c342d5201265a80
robuntu <robuntu@stro.ycdsb.ca>
2018-12-12 10:22:38 -0500

    my file

---
+++ RRTA-Kun.py
@@ -0,0 +1,13 @@
+import kivy
+kivy.require('1.9.0')
+
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+
+class GridLayoutApp(App):
+
+    def build(self):
+        return GridLayout()
+
+glApp = GridLayoutApp()
+glApp.run()
