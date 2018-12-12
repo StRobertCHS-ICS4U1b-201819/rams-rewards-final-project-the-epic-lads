cc4f9852ac06d5704ed2cf0684e0128544003f41
Your Name <you@example.com>
2018-12-12 10:41:17 -0500

    picture

--- RRTA-Leo.py
+++ RRTA-Leo.py
@@ -2,7 +2,8 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.core.window import Window
-Window.clearcolor = (1, 0, 1, 0)
+from kivy.uix.image import Image
+Window.clearcolor = (1, 1, 1, 1)
 
 # Create both screens. Please note the root.manager.current: this is how
 # you can control the ScreenManager from kv. Each screen has by default a
@@ -12,10 +13,8 @@ Builder.load_string("""
     GridLayout:
         cols: 2
         rows: 1
-
-       
-        padding: 55
-        
+        padding: 27
+        spacing: 27
         Button:
             text: 'Goto settings'
             on_press: root.manager.current = 'settings'
@@ -27,24 +26,38 @@ Builder.load_string("""
     GridLayout:
         cols: 2
         rows: 1
+        padding: 27
+        spacing: 27
         Button:
             text: 'My settings button'
             on_press: root.manager.current = 'leo'
         Button:
             text: 'Back to menu'
             on_press: root.manager.current = 'menu'
-            
-<LeoScreen>:
+
+<ScanQRCodeScreen>:
     GridLayout:
         cols: 2
         rows: 1
+        padding: 27
+        spacing: 27
         Button:
             text: 'scan QR code'
             on_press: root.manager.current = 'settings'
         Button:
             text: 'Back to menu'
             on_press: root.manager.current = 'menu'
+
+<-FullImage>:
+    canvas:
+        Color:
+            rgb: (1, 1, 1)
+        Rectangle:
+            texture: self.texture
+            size: self.width + 20, self.height + 20
+            pos: self.x - 10, self.y - 10
 """)
+
 
 # Declare both screens
 
@@ -57,15 +70,23 @@ class SettingsScreen(Screen):
     pass
 
 
-class LeoScreen(Screen):
+class ScanQRCodeScreen(Screen):
     pass
+
+
+class FullImage(Image):
+    pass
+
+
+#class QrPicScreen(Screen):
+#    pass
 
 
 # Create the screen manager
 sm = ScreenManager()
 sm.add_widget(MenuScreen(name='menu'))
 sm.add_widget(SettingsScreen(name='settings'))
-sm.add_widget(LeoScreen(name='leo'))
+sm.add_widget(KunScreen(name='leo'))
 
 
 class TestApp(App):
