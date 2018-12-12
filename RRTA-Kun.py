d9ca1b5e55d2247347522fabd79762d91b3ecf42
robuntu <robuntu@stro.ycdsb.ca>
2018-12-12 11:07:13 -0500

    copying into a new brach

--- RRTA-Kun.py
+++ RRTA-Kun.py
@@ -2,7 +2,7 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.core.window import Window
-
+from kivy.uix.image import Image
 Window.clearcolor = (1, 1, 1, 1)
 
 # Create both screens. Please note the root.manager.current: this is how
@@ -16,26 +16,12 @@ Builder.load_string("""
         padding: 27
         spacing: 27
         Button:
-            text: 'Go to settings'
-            on_press: root.manager.current = 'settings'
+            text: 'Scan QR Code'
+            on_press: root.manager.current = 'scanQRcode'
         Button:
             text: "Exit"
             on_press: app.stop() 
-
-<SettingsScreen>:
-    GridLayout:
-        cols: 2
-        rows: 1
-        padding: 27
-        spacing: 27
-        Button:
-            text: 'My settings button'
-            on_press: root.manager.current = 'leo'
-        Button:
-            text: 'Back to menu'
-            on_press: root.manager.current = 'menu'
-
-<ScanQRCodeSCreen>:
+<ScanQRCodeScreen>:
     GridLayout:
         cols: 2
         rows: 1
@@ -43,10 +29,18 @@ Builder.load_string("""
         spacing: 27
         Button:
             text: 'scan QR code'
-            on_press: root.manager.current = 'settings'
+            on_press: root.manager.current = 'qrimage'
         Button:
             text: 'Back to menu'
             on_press: root.manager.current = 'menu'
+<-FullImage>:
+    canvas:
+        Color:
+            rgb: (1, 1, 1)
+        Rectangle:
+            texture: self.texture
+            size: self.width + 20, self.height + 20
+            pos: self.x - 10, self.y - 10
 """)
 
 
@@ -57,19 +51,22 @@ class MenuScreen(Screen):
     pass
 
 
-class SettingsScreen(Screen):
+class ScanQRCodeScreen(Screen):
     pass
 
 
-class scanQRCodeScreen(Screen):
+class FullImage(Image):
     pass
+
+
+#class QrPicScreen(Screen):
+#    pass
 
 
 # Create the screen manager
 sm = ScreenManager()
 sm.add_widget(MenuScreen(name='menu'))
-sm.add_widget(SettingsScreen(name='settings'))
-sm.add_widget(scanQRCodeScreen(name='leo'))
+sm.add_widget(ScanQRCodeScreen(name='scanQRcode'))
 
 
 class TestApp(App):
