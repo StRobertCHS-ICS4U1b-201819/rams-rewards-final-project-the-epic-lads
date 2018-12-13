fe986aa6f15470da3c602b124592470d2a29c022
robuntu <robuntu@stro.ycdsb.ca>
2018-12-13 11:06:07 -0500

    done class

--- RRTA-Kun.py
+++ RRTA-Kun.py
@@ -5,6 +5,8 @@ from kivy.core.window import Window
 from kivy.uix.image import Image
 Window.clearcolor = (1, 1, 1, 1)
 
+
+
 # Create both screens. Please note the root.manager.current: this is how
 # you can control the ScreenManager from kv. Each screen has by default a
 # property manager that gives you the instance of the ScreenManager used.
@@ -15,6 +17,8 @@ Builder.load_string("""
         rows: 1
         padding: 27
         spacing: 27
+        Button:
+            text: 'Rams'
         Button:
             text: 'Scan QR Code'
             on_press: root.manager.current = 'scanQRcode'
@@ -29,18 +33,12 @@ Builder.load_string("""
         spacing: 27
         Button:
             text: 'scan QR code'
-            on_press: root.manager.current = 'qrimage'
+            on_press: root.manager.current = 'qrpic'
         Button:
             text: 'Back to menu'
             on_press: root.manager.current = 'menu'
-<-FullImage>:
-    canvas:
-        Color:
-            rgb: (1, 1, 1)
-        Rectangle:
-            texture: self.texture
-            size: self.width + 20, self.height + 20
-            pos: self.x - 10, self.y - 10
+<QRPicScreen>:
+
 """)
 
 
@@ -59,14 +57,14 @@ class FullImage(Image):
     pass
 
 
-#class QrPicScreen(Screen):
-#    pass
-
+class QrPicScreen(Screen):
+    pass
 
 # Create the screen manager
 sm = ScreenManager()
 sm.add_widget(MenuScreen(name='menu'))
 sm.add_widget(ScanQRCodeScreen(name='scanQRcode'))
+sm.add_widget(QrPicScreen(name='qrpic'))
 
 
 class TestApp(App):
