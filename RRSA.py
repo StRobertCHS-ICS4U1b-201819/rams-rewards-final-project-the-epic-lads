f60cc6001c1d1c419200f4c0b0fcf28aa9e5f513
AntAndVar <anton.v@live.ca>
2018-12-14 17:09:35 -0500

    added buttons fixed quit

--- RRSA.py
+++ RRSA.py
@@ -1,10 +1,6 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
-
-# Create both screens. Please note the root.manager.current: this is how
-# you can control the ScreenManager from kv. Each screen has by default a
-# property manager that gives you the instance of the ScreenManager used.
 Builder.load_string("""
 <MainScreen>:
     BoxLayout:
@@ -12,26 +8,65 @@ Builder.load_string("""
             text: 'Check information'
             on_press: root.manager.current = 'view'
         Button:
+            text: 'Update Information'
+            on_press: root.manager.current = 'update'
+        Button:
+            text: 'View QR Code'
+            on_press: root.manager.current = 'qr'
+        Button:
+            text: 'View History'
+            on_press: root.manager.current = 'history'
+        Button:
             text: 'Quit'
+            on_press: App.get_running_app().stop() 
 
 <InformationScreen>:
     BoxLayout:
         Button:
             text: 'Back'
             on_press: root.manager.current = 'main'
+
+<UpdateScreen>:
+    BoxLayout:
+        Button:
+            text: 'Back'
+            on_press: root.manager.current = 'main'
+
+<QRScreen>:
+    BoxLayout:
+        Button:
+            text: 'Back'
+            on_press: root.manager.current = 'main'
+
+<HistoryScreen>:
+    BoxLayout:
+        Button:
+            text: 'Back'
+            on_press: root.manager.current = 'main'
 """)
 
-# Declare both screens
+
 class MainScreen(Screen):
     pass
 
 class InformationScreen(Screen):
     pass
 
-# Create the screen manager
+class UpdateScreen(Screen):
+    pass
+
+class QRScreen(Screen):
+    pass
+
+class HistoryScreen(Screen):
+    pass
+
 sm = ScreenManager()
 sm.add_widget(MainScreen(name='main'))
 sm.add_widget(InformationScreen(name='view'))
+sm.add_widget(UpdateScreen(name='update'))
+sm.add_widget(QRScreen(name='qr'))
+sm.add_widget(HistoryScreen(name='history'))
 
 class TestApp(App):
 
