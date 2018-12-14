9d3456c78d2d0111ca1db1554177de57769fdeda
Rezhe <rzwang.ca@gmail.com>
2018-12-14 01:21:31 -0500

    as a web page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,176 @@
 <template>
-  <div>
+<div style="background-color:#E3E5E7;">
+
+    <div style="margin:0 auto;max-width:600px;background:#FF8A2BE2;">
+      <table cellpadding="0" cellspacing="0" style="font-size:0px;width:100%;background:rgba(241,241,241,0.8);" align="center" border="0">
+        <tbody>
+          <tr>
+            <td style="text-align:center;vertical-align:top;font-size:0px;padding:20px 0px;">
+              <div aria-labelledby="mj-column-per-100" class="mj-column-per-100" style="vertical-align:top;display:inline-block;font-size:13px;text-align:left;width:100%;">
+                <table cellpadding="0" cellspacing="0" style="vertical-align:top;" width="100%" border="0">
+                  <tbody>
+                    <tr>
+                      <td style="word-break:break-word;font-size:0px;padding:10px 25px;" align="center">
+                        <table cellpadding="0" cellspacing="0" style="border-collapse:collapse;border-spacing:0px;" align="center" border="0">
+                          <tbody>
+                            <tr>
+                              <td style="width:80px;"><a href="about:blank" target="_blank"><img alt="auth0" title="" height="auto" src="https://pbs.twimg.com/profile_images/1541158612/YCDSB_Logo_400x400.png" style="border:none;border-radius:;display:block;outline:none;text-decoration:none;width:100%;height:auto;" width="80"></a></td>
+                            </tr>
+                          </tbody>
+                        </table>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
+    <div style="margin:0 auto;max-width:600px;background:#222228;">
+      <table cellpadding="0" cellspacing="0" style="font-size:0px;width:100%;background:#222228;" align="center" border="0">
+        <tbody>
+          <tr>
+            <td style="text-align:center;vertical-align:top;font-size:0px;padding:20px 0px;">
+
+              <div aria-labelledby="mj-column-per-80" class="mj-column-per-80" style="vertical-align:top;display:inline-block;font-size:13px;text-align:left;width:100%;">
+                <table cellpadding="0" cellspacing="0" style="vertical-align:top;" width="100%" border="0">
+                  <tbody>
+                    <tr>
+                      <td style="word-break:break-word;font-size:0px;padding:10px 25px;padding-top:30px;" align="center">
+                        <table cellpadding="0" cellspacing="0" style="border-collapse:collapse;border-spacing:0px;" align="center" border="0">
+                          <tbody>
+                            <tr>
+                              <td style="width:80px;"><qrcode-stream height="120" style="border:none;border-radius:;display:block;outline:none;text-decoration:none;width:100%;height:auto;" width="120" @decode="onDecode" @init="onInit" /></td>
+                            </tr>
+                          </tbody>
+                        </table>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td style="word-break:break-word;font-size:0px;padding:0px 20px 0px 20px;" align="center">
+                        <div style="cursor:auto;color:white;font-family:'Avenir Next', Avenir, sans-serif;font-size:32px;line-height:60ps;text-align:center">
+                          Scan the QR code<br>
+                          <p class="error">{{ error }}</p>
+                          <p class="decode-result"><b>{{ result }}</b></p>
+                        </div>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div style="margin:0 auto;max-width:600px;background:white;">
+      <table cellpadding="0" cellspacing="0" style="font-size:0px;width:100%;background:white;" align="center" border="0">
+        <tbody>
+          <tr>
+            <td style="text-align:center;vertical-align:top;font-size:0px;padding:0px 30px;">
+
+              <p style="font-size:1px;margin:0 auto;border-top:1px solid #E3E5E7;width:100%;"></p>
+
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div style="margin:0 auto;max-width:600px;background:white;">
+      <table cellpadding="0" cellspacing="0" style="font-size:0px;width:100%;background:white;" align="center" border="0">
+        <tbody>
+          <tr>
+            <td style="text-align:center;vertical-align:top;font-size:0px;padding:20px 0px;">
+              <div aria-labelledby="mj-column-per-100" class="mj-column-per-100" style="vertical-align:top;display:inline-block;font-size:13px;text-align:left;width:100%;">
+                <table cellpadding="0" cellspacing="0" width="100%" border="0">
+                  <tbody>
+                    <tr>
+                      <td style="word-break:break-word;font-size:0px;padding:10px 25px;" align="center">
+                        <table cellpadding="0" cellspacing="0" align="center" border="0">
+                          <tbody>
+                            <tr>
+                              <td v-if="!authenticated" @click="login()" style="border-radius:3px;color:white;cursor:auto;" align="center" valign="middle" bgcolor="#EB5424">
+                                <a style="display:inline-block;text-decoration:none;background:#EB5424;border-radius:3px;color:white;font-family:'Avenir Next', Avenir, sans-serif;font-size:14px;font-weight:500;line-height:35px;padding:10px 25px;margin:0px;"
+                                  >
+                                                                    LOGIN TO YOUR ACCOUNT
+                                                                </a>
+                                                                </td>
+                                <td v-if="authenticated" @click="privateMessage()" style="border-radius:3px;color:white;cursor:auto;" align="center" valign="middle" bgcolor="#EB5424">
+                                <a style="display:inline-block;text-decoration:none;background:#EB5424;border-radius:3px;color:white;font-family:'Avenir Next', Avenir, sans-serif;font-size:14px;font-weight:500;line-height:35px;padding:10px 25px;margin:0px;">Call Private </a> </td>
+                            <div class="divider"/>
+                            <td  v-if="authenticated" @click="logout()" style="border-radius:3px;color:white;cursor:auto;" align="center" valign="middle" bgcolor="#EB5424">
+                                <a style="display:inline-block;text-decoration:none;background:#EB5424;border-radius:3px;color:white;font-family:'Avenir Next', Avenir, sans-serif;font-size:14px;font-weight:500;line-height:35px;padding:10px 25px;margin:0px;">Log Out </a> </td>
+                            </tr>
+                          </tbody>
+                        </table>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
+    <div style="margin:0 auto;max-width:600px;background:white;">
+      <table cellpadding="0" cellspacing="0" style="font-size:0px;width:100%;background:white;" align="center" border="0">
+        <tbody>
+          <tr>
+            <td style="text-align:center;vertical-align:top;font-size:0px;padding:20px 0px;">
+
+              <div aria-labelledby="mj-column-per-100" class="mj-column-per-100" style="vertical-align:top;display:inline-block;font-size:13px;text-align:left;width:100%;">
+                <table cellpadding="0" cellspacing="0" style="vertical-align:top;" width="100%" border="0">
+                  <tbody>
+                    <tr>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
+    <div style="margin:0 auto;max-width:600px;background:#F5F7F9;">
+      <table cellpadding="0" cellspacing="0" style="font-size:0px;width:100%;background:#F5F7F9;" align="center" border="0">
+        <tbody>
+          <tr>
+            <td style="text-align:center;vertical-align:top;font-size:0px;padding:20px 0px;">
+
+              <div aria-labelledby="mj-column-per-100" class="mj-column-per-100" style="vertical-align:top;display:inline-block;font-size:13px;text-align:left;width:100%;">
+                <table cellpadding="0" cellspacing="0" style="vertical-align:top;" width="100%" border="0">
+                  <tbody>
+                    <tr>
+                      <td style="word-break:break-word;font-size:0px;padding:0px 20px;" align="center">
+                        <div style="cursor:auto;color:#222228;font-family:'Avenir Next', Avenir, sans-serif;font-size:13px;line-height:20px;">
+                          <p class="error">{{ error }}</p>
+                          <p class="decode-result">Last result: <b>{{ result }}</b></p>
+                        </div>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
+    <div></div>
+
+  </div>
+  <!--div>
     <div>
       <p class="error">{{ error }}</p>
       <p class="decode-result">Last result: <b>{{ result }}</b></p>
@@ -27,7 +198,7 @@
     </md-button>
     {{message}}
     <br>
-  </div>
+  </div-->
 </template>
 <script>
 import AuthService from './auth/AuthService'
@@ -107,5 +278,14 @@ export default {
 .error {
   font-weight: bold;
   color: red;
+}
+.qrcode-stream{
+  position: flex;
+  width:100%;
+}
+.divider{
+    width:15px;
+    height:auto;
+    display:inline-block;
 }
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,11 +2,8 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import VueMaterial from 'vue-material'
-import 'vue-material/dist/vue-material.min.css'
 import VueQrcodeReader from 'vue-qrcode-reader'
 Vue.config.productionTip = false
-Vue.use(VueMaterial)
 Vue.use(VueQrcodeReader)
 /* eslint-disable no-new */
 new Vue({
