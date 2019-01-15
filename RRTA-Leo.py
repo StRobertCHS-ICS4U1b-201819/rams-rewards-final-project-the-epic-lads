0c809e80ce629b167f06d9bba2002c343a6afe3c
Lxiao1 <36396256+Lxiao1@users.noreply.github.com>
2019-01-15 11:09:42 -0500

    Update RRTA-Leo.py

--- RRTA-Leo.py
+++ RRTA-Leo.py
@@ -2,23 +2,36 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.core.window import Window
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import ObjectProperty
+from kivy.uix.listview import ListItemButton
 from kivy.uix.image import Image
-Window.clearcolor = (1, 1, 1, 1)
 
+Window.clearcolor = (0, 0.7, 1, 1)
+
+# Create both screens. Please note the root.manager.current: this is how
+# you can control the ScreenManager from kv. Each screen has by default a
+# property manager that gives you the instance of the ScreenManager used.
 Builder.load_string("""
 <MenuScreen>:
     GridLayout:
-        cols: 2
-        rows: 1
-        padding: 27
-        spacing: 27
-        Button:
-            text: 'Scan QR Code'
-            on_press: root.manager.current = 'scanQRcode'
-        Button:
-            text: "Exit"
-            on_press: app.stop() 
-
+        rows: 2
+        BoxLayout:
+            padding: 10
+            Button:
+                background_color: 0, 0, 250, 255
+                font_size: 32
+                text: 'Rams Rewards'
+        BoxLayout:
+            spacing: 10
+            padding: 10
+            Button: 
+                text: 'Scan QR Code'
+                on_press: root.manager.current = 'scanQRcode'
+            Button:
+                text: "Exit"
+                on_press: app.stop() 
 <ScanQRCodeScreen>:
     GridLayout:
         cols: 2
@@ -26,90 +39,124 @@ Builder.load_string("""
         padding: 27
         spacing: 27
         Button:
-            text: 'scan QR code'
-            on_press: root.manager.current = 'addsubpoints'
+            text: 'Reward Your Students'
+            on_press: root.manager.current = 'rewarding'
         Button:
             text: 'Back to menu'
             on_press: root.manager.current = 'menu'
-            
-<AddSubScreen>:
-    BoxLayout:
-        Button:
-            text: "Add Points"
-            on_press: root.manager.current = 'calculate'
-        Button:
-            text: "Deduct Points"
 
-<CalcGridLayoutScreen>:
+<RewardingScreen>:
     GridLayout:
-        cols:3
-        rows:4
+        id: rewarding
+        display: entry
+        rows: 4
+        spacing: 10
+        padding: 10
 
-        Button:
-            text: 'Join a club (5 points)'
-            padding: 25,0
-            text_size: 250,None
-            font_size: 20
-        Button:
-            text: 'Make it in a athletic team (10 points)'
-            padding: 25,0
-            text_size: 250,None
-            font_size: 20
+        BoxLayout:
+            Button: 
+                text: 'Current Points:'
+                background_color: 0,0,0,0
+                font_color: 0,0,0,0
+                font_size: 28
+                size_hint_x: 0.35
+                width: 100
+            TextInput:
+                id: entry
+                font_size: 32
+                multiline: False
+                readonly: True
 
-        Button:
-            text: 'Attend a club/team meeting (1 point)'
-            padding: 25,0
-            text_size: 250,None
-            font_size: 20
+        BoxLayout:
+            spacing: 10
+            Button:
+                text: "Join a Club (5 pts)"
+                padding: 25,0
+                font_size: 12
+                on_press: entry.text = str(eval(entry.text + '+ 5'))
 
-        Button:
-            text: 'Donate $5 for school charity events (once per event) (3 points)'
-            padding: 25,0
-            text_size: 250,None
-            font_size: 20
+            Button:
+                text: 'Join Athletic Team (10 pts)'
+                padding: 25,0
+                font_size: 12
+                on_press: entry.text = str(eval(entry.text + '+ 10'))
+            Button:
+                text: 'Attend a club/team meeting (1 pts)'
+                padding: 25,0
+                font_size: 11
+                on_press: entry.text = str(eval(entry.text + '+ 1'))
+            Button:
+                text: 'Donate $5 for school (3 points)'
+                padding: 25,0
+                font_size: 12
+                on_press: entry.text = str(eval(entry.text + '+ 3'))
+        BoxLayout:
+            spacing: 10
+            Button:
+                text: 'Top 3 in cafeteria Kahoot (3 pts)'
+                padding: 25,0
+                font_size: 12
+                on_press: entry.text = str(eval(entry.text + '+ 3'))
+            Button:
+                text: 'Participate Terry Fox Trot (5 pts)'
+                padding: 25, 0
+                font_size: 12
+                on_press: entry.text = str(eval(entry.text + '+ 5'))
+            Button:
+                text: 'Unit Test Mark: 85+ (2 pts)'
+                padding: 25,0
+                font_size: 12
+                on_press: entry.text = str(eval(entry.text + '+ 2'))
+            Button:
+                text: 'Course Mark: 80+(4 pts)'
+                padding: 25,0
+                font_size: 12
+                on_press: entry.text = str(eval(entry.text + '+ 4'))
+        BoxLayout:
+            spacing: 10
+            Button:
+                text: 'Course Mark: 90+ (5 pts)'
+                padding: 25,0
+                font_size: 12
+                on_press: entry.text = str(eval(entry.text + '+ 5'))
+            Button:
+                text: 'Course Mark: 95+ (6 pts)'
+                padding: 25,0
+                font_size: 12
+                on_press: entry.text = str(eval(entry.text + '+ 6'))
+            Button:
+                text: 'Deduct pts (-1 pts)'
+                font_size: 12
+                on_press: entry.text = str(eval(entry.text + '- 1'))
+            Button:
+                text: 'Save and Quit'
+                font_size: 12
 
-        Button:
-            text: 'Place top 3 in cafeteria kahoot games (3 points)'
-            padding: 25,0
-            text_size: 250,None
-            font_size: 20
 
-        Button:
-            text: 'Participate in the Terry Fox turkey trot (5 points)'
-            padding: 25,0
-            text_size: 250,None
-            font_size: 20
-        Button:
-            text: 'Achieve a unit test mark of 85+ (2 points)'
-            padding: 25,0
-            text_size: 250,None
-            font_size: 20
 
-        Button:
-            text: 'Achieve a course mark of 80 (4 points)'
-            padding: 25,0
-            text_size: 250,None
-            font_size: 20
 
-        Button:
-            text: 'Achieve a course mark of 90 (5 points)'
-            padding: 25,0
-            text_size: 250,None
-            font_size: 20
-
-        Button:
-            text: 'Achieve a course mark of 95+ (6 points)'
-            padding: 25,0
-            text_size: 250,None
-            font_size: 20
-
-        Button:
-            text: 'Deduct points'
-            font_size: 20
-
+<AreYouSureScreen>:
+    GridLayout:
+        rows: 2
+        BoxLayout:
+            padding: 10
+            Button:
+                background_color: 250, 0, 0, 25
+                font_size: 32
+                text: 'Are you sure about that?'
+        BoxLayout:
+            spacing: 10
+            padding: 10
+            Button: 
+                text: 'Yes'
+                on_press: root.manager.current = 'scanQRcode' 
+            Button:
+                text: "No"
+                on_press: root.manager.current = 'rewarding' 
 """)
 
 
+# Declare screens
 class MenuScreen(Screen):
     pass
 
@@ -118,24 +165,20 @@ class ScanQRCodeScreen(Screen):
     pass
 
 
-class RewardsScreen(Screen):
+class RewardingScreen(Screen):
     pass
 
 
-class AddSubScreen(Screen):
+class AreYouSureScreen(Screen):
     pass
 
-
-class CalcGridLayoutScreen(Screen):
-    pass
 
 # Create the screen manager
 sm = ScreenManager()
 sm.add_widget(MenuScreen(name='menu'))
 sm.add_widget(ScanQRCodeScreen(name='scanQRcode'))
-sm.add_widget(AddSubScreen(name='addsubpoints'))
-sm.add_widget(RewardsScreen(name='rewards'))
-sm.add_widget(CalcGridLayoutScreen(name='calculate'))
+sm.add_widget(RewardingScreen(name='rewarding'))
+sm.add_widget(AreYouSureScreen(name='areyousure'))
 
 
 class TestApp(App):
@@ -146,32 +189,3 @@ class TestApp(App):
 
 if __name__ == '__main__':
     TestApp().run()
-"""
-< RewardsScreen >:
-GridLayout:
-cols: 3
-rows: 3
-font_size: 20
-Button:
-text: '1'
-font_size: 20
-
-Button:
-text: '2'
-font_size: 20
-Button:
-text: '3'
-font_size: 20
-Button:
-text: '4'
-font_size: 20
-Button:
-text: '5'
-font_size: 20
-Button:
-text: '6'
-font_size: 20
-Button:
-text: '7'
-font_size: 20     
-"""
